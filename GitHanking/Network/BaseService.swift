5be49dfae929889107c26ff920f8c7aa836bc3e5
Guadalupe Montefusco <guadalupesophya@icloud.com>
2020-08-22 21:03:46 -0300

    Realizada a integração com API

---
+++ GitHanking/Network/BaseService.swift
@@ -0,0 +1,9 @@
+//
+//  BaseService.swift
+//  GitHanking
+//
+//  Created by Guadalupe Montefusco de Oliveira on 22/08/20.
+//  Copyright © 2020 Guadalupe Montefusco de Oliveira. All rights reserved.
+//
+
+import Foundation
---
+++ GitHanking/Network/GitHubError.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubError.swift
+//  GitHanking
+//
+//  Created by Guadalupe Montefusco de Oliveira on 22/08/20.
+//  Copyright © 2020 Guadalupe Montefusco de Oliveira. All rights reserved.
+//
+
+enum GitHubError {
+    case parser
+    case exception(GitHubErrorData)
+}
+
